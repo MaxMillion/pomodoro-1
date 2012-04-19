fc9f705ae4f5eedfab2cdd293ab8a780b6aea208
Lucas <lukspn.27@gmail.com>
2012-04-19 17:10:25 -0300

    Arlam working and tests added

--- javascripts/pomodoro.js
+++ javascripts/pomodoro.js
@@ -1,38 +1,46 @@
 var Pomodoro = function(){
+  this.timeout_sound = null;
+  this.timeout_red_css = null;
 
   /* Função de initialize do Pomodoro */
   this.init = function(){
-    var that = this
+    var that = this;
+
 
     $("button").click(function(){
       if (!$(this).hasClass("clicked")){
-        var data_time   = $(this).attr("data-time") || 0
-        var miliseconds = Date.now() + (data_time * 60 * 1000);
-        var counter     = parseInt($(this).attr("data-count"));
-
+        var counter = parseInt($(this).attr("data-count"));
         $(this).attr("data-count", counter+=1);
-        that.setTime(miliseconds, this);
+        that.setTime(this);
       }
     });
-
     this.setTime();
   }
 
   /* Inicia o Countdown */
-  this.setTime = function(time, button){
-    var that = this
+  this.setTime = function(button){
+    var that = this;
+
+    clearTimeout(this.timeout_sound);
+    clearTimeout(this.timeout_red_css);
+    var data_time   = $(button).attr("data-time") || 0;
+    var miliseconds = (data_time * 60 * 1000);
+    var time_in_miliseconds = Date.now() + miliseconds;
 
     this.buttonState(button, "clicked");
-    if (time === undefined){ time = 0; }
-    if(button !== undefined) { this.history(button); }
+    if(button !== undefined){ this.history(button); }
 
+    if ($("#countdown").hasClass("clock_red")) { this.changeColor(); }
     $('#countdown').countdown('destroy');
     $("#countdown").countdown({
-      until: new Date(time),
+      until: new Date(time_in_miliseconds),
       format: 'MS',
-      layout: "{mnn}{sep}{snn}",
-      onExpiry: function(){ that.buttonFinished(); }
+      layout: "{mnn}{sep}{snn}"
     });
+    if ($(button).attr("name") != "Reset" && button !== undefined){
+      this.timeout_sound = setTimeout(function(){that.buttonFinished()}, miliseconds);
+      this.timeout_red_css = setTimeout(function(){that.changeColor()}, (miliseconds - 15000));
+    }
   }
 
   /* Toda vez que um botão for clicado, joga as informações sobre ele no histórico */
@@ -62,12 +70,25 @@ var Pomodoro = function(){
     if (button !== undefined){
       $('button[name!="'+button.name+'"]').removeClass();
       $(button).removeClass().addClass(state);
+      this.playAlarm($(button));
     }
   }
 
   /* Muda a classe do botão que está clicado pra finalizado */
   this.buttonFinished = function(){
-    $("button.clicked").toggleClass("clicked finished");
+    var button = $("button.clicked");
+    button.toggleClass("clicked finished");
+    this.playAlarm(button);
+  }
+
+  this.playAlarm = function (button){
+    if (button !== undefined){
+      $('#sound_element').html("<embed src=sounds/" + button.attr("class") + ".wav hidden=true autostart=true loop=false>");
+    }
+  }
+
+  this.changeColor = function (){
+    $('#countdown').toggleClass("clock_red");
   }
 
   this.init();
--- tests/spec/PomodoroSpec.js
+++ tests/spec/PomodoroSpec.js
@@ -4,8 +4,8 @@ describe("Pomodoro", function(){
 
   beforeEach(function() {
     jasmine.getFixtures().set(
-      '<button name="Pomodoro" data-time="25" data-count="0"> Pomodoro </button> <button name="Short Break" data-time="5" data-count="0"> Short Break </button> <button name="Long Break" data-time="10" data-count="0"> Long Break </button> <button name="Reset" data-text="You pressed "> Reset </button> <ul class="history"></ul> <span id="countdown"></span>'
-    );
+      '<button name="Pomodoro" data-time="25" data-count="0"> Pomodoro </button> <button name="Short Break" data-time="5" data-count="0"> Short Break </button> <button name="Long Break" data-time="10" data-count="0"> Long Break </button> <button name="Reset" data-text="You pressed "> Reset </button> <ul class="history"></ul> <div id="clock"></div> <span id="countdown"></span> <div id="sound_element"></div>'
+      );
 
     pomodoro = new Pomodoro();
   });
@@ -20,19 +20,41 @@ describe("Pomodoro", function(){
       expect( button ).toHaveClass("clicked");
     });
 
+    it("should have the 'embed' tag when clicked", function() {
+      var sound_element = $("#sound_element")
+      expect( sound_element ).toContain("embed");
+    });
+
+    it("should have the clicked.wav sound", function() {
+      var embed = $("#sound_element embed");
+      expect ( embed ).toHaveAttr("src", "sounds/clicked.wav");
+    })
+
     it("should has the class 'finished' when clicked and the time's over", function() {
       pomodoro.buttonFinished();
       expect( button ).toHaveClass("finished");
     });
+
+    it("should have the finished.wav sound", function() {
+      pomodoro.buttonFinished();
+      var embed = $("#sound_element embed");
+      expect( embed ).toHaveAttr("src", "sounds/finished.wav");
+    })
 
     it("should start the countdown when clicked", function() {
       var countdown = $("#countdown")
       expect( countdown ).toHaveText("25:00");
     });
 
+    it("should get the countdown numbers red when time is equal to 15 seconds", function() {
+      pomodoro.changeColor();
+      var countdown = $("#countdown");
+      expect( countdown ).toHaveClass("clock_red");
+    })
+
     it("should create a entry on the history when clicked", function() {
-      var history       = $("ul.history")
-      var history_entry = history.find("li")
+      var history       = $("ul.history");
+      var history_entry = history.find("li");
 
       expect( history ).toContain("li");
       expect( history_entry ).toContain("time");
@@ -49,19 +71,41 @@ describe("Pomodoro", function(){
       expect( button ).toHaveClass("clicked");
     });
 
+    it("should have the 'embed' tag when clicked", function() {
+      var sound_element = $("#sound_element")
+      expect( sound_element ).toContain("embed");
+    });
+
+    it("should have the clicked.wav sound", function() {
+      var embed = $("#sound_element embed");
+      expect ( embed ).toHaveAttr("src", "sounds/clicked.wav");
+    })
+
     it("should has the class 'finished' when clicked and the time's over", function() {
       pomodoro.buttonFinished();
       expect( button ).toHaveClass("finished");
     });
+
+    it("should have the finished.wav sound", function() {
+      pomodoro.buttonFinished();
+      var embed = $("#sound_element embed");
+      expect( embed ).toHaveAttr("src", "sounds/finished.wav");
+    })
 
     it("should start the countdown when clicked", function() {
       var countdown = $("#countdown")
       expect( countdown ).toHaveText("05:00");
     });
 
+    it("should get the countdown numbers red when time is equal to 15 seconds", function() {
+      pomodoro.changeColor();
+      var countdown = $("#countdown");
+      expect( countdown ).toHaveClass("clock_red");
+    })
+
     it("should create a entry on the history when clicked", function() {
-      var history       = $("ul.history")
-      var history_entry = history.find("li")
+      var history       = $("ul.history");
+      var history_entry = history.find("li");
 
       expect( history ).toContain("li");
       expect( history_entry ).toContain("time");
@@ -78,22 +122,44 @@ describe("Pomodoro", function(){
       expect( button ).toHaveClass("clicked");
     });
 
+    it("should have the 'embed' tag when clicked", function() {
+      var sound_element = $("#sound_element")
+      expect( sound_element ).toContain("embed");
+    });
+
+    it("should have the clicked.wav sound", function() {
+      var embed = $("#sound_element embed");
+      expect ( embed ).toHaveAttr("src", "sounds/clicked.wav");
+    })
+
     it("should has the class 'finished' when clicked and the time's over", function() {
       pomodoro.buttonFinished();
       expect( button ).toHaveClass("finished");
     });
+
+    it("should have the finished.wav sound", function() {
+      pomodoro.buttonFinished();
+      var embed = $("#sound_element embed");
+      expect( embed ).toHaveAttr("src", "sounds/finished.wav");
+    })
 
     it("should start the countdown when clicked", function() {
       var countdown = $("#countdown")
       expect( countdown ).toHaveText("10:00");
     });
 
+    it("should get the countdown numbers red when time is equal to 15 seconds", function() {
+      pomodoro.changeColor();
+      var countdown = $("#countdown");
+      expect( countdown ).toHaveClass("clock_red");
+    })
+
     it("should create a entry on the history when clicked", function() {
       var history       = $("ul.history")
       var history_entry = history.find("li")
 
       expect( history ).toContain("li");
-      expect( history_entry ).toContain("time");
+    expect( history_entry ).toContain("time");
     });
   });
 
@@ -107,14 +173,29 @@ describe("Pomodoro", function(){
       expect( button ).toHaveClass("clicked");
     });
 
+    it("should have the 'embed' tag when clicked", function() {
+      var sound_element = $("#sound_element")
+      expect( sound_element ).toContain("embed");
+    });
+
+    it("should have the clicked.wav sound", function() {
+      var embed = $("#sound_element embed");
+      expect ( embed ).toHaveAttr("src", "sounds/clicked.wav");
+    })
+
     it("should start the countdown when clicked", function() {
       var countdown = $("#countdown")
       expect( countdown ).toHaveText("00:00");
     });
 
+    it("should not have the countdown numbers red when clicked", function() {
+      var countdown = $("#countdown");
+      expect( countdown ).not.toHaveClass("clock_red");
+    });
+
     it("should create a entry on the history when clicked", function() {
-      var history       = $("ul.history")
-      var history_entry = history.find("li")
+      var history       = $("ul.history");
+      var history_entry = history.find("li");
 
       expect( history ).toContain("li");
       expect( history_entry ).toContain("time");
